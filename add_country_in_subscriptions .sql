d740cb6e40271ece303bd3cbc1578bc3462cc49d
Brishti Sutradhar <139124898+BrishtiSutradhar@users.noreply.github.com>
2023-10-04 21:15:13 +0100

    Update add_country_in_subscriptions .sql

--- add_country_in_subscriptions .sql
+++ add_country_in_subscriptions .sql
@@ -2,10 +2,9 @@
 SELECT
   *
 FROM
-  brishti-project.home_exchange.subscriptions AS sp
+  lewagon-final-project-397408.home_exchange.subscriptions AS sp
 LEFT JOIN
- brishti-project.home_exchange.country_code AS cp
+ lewagon-final-project-397408.home_exchange.countryCode AS cp
 ON
   cp.codeCountry = sp.country
-
  
